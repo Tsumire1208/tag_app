b655f93a9be8b613ca1dbff56f70a4e89a66eb2b
Tsumire1208 <utamiori20210117@gmail.com>
2023-02-24 21:46:20 +0900

    インクリメンタルサーチ　非同期通信前　処理実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,7 +37,12 @@ class PostsController < ApplicationController
     tag.save if tag_name.present?
     post.update(params)
     PostTagRelation.create(post_id: post.id, tag_id: tag.id) if tag_name.present?
-    end
+  end
+
+  def search
+    return nil if params[:keyword] == ""
+    tag = Tag.where(['tag_name LIKE ?', "%#{params[:keyword]}%"] )
+    render json:{ keyword: tag }
   end
 
   private
