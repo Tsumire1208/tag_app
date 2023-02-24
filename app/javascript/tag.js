10f69a738f35efd15751e18c4f6cfa763b82dfb4
Tsumire1208 <utamiori20210117@gmail.com>
2023-02-24 22:03:18 +0900

    非同期通信後処理

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -8,7 +8,23 @@ document.addEventListener("DOMContentLoaded", () => {
         XHR.open("GET", `/posts/search/?keyword=${keyword}`, true);
         XHR.responseType = "json";
         XHR.send();
-
+        XHR.onload = () => {
+          const tagName = XHR.response.keyword;
+          const searchResult = document.getElementById("search-result");
+          searchResult.innerHTML = "";
+          tagName.forEach((tag) => {
+            const childElement = document.createElement("div");
+            childElement.setAttribute("class", "child");
+            childElement.setAttribute("id", tag.id);
+            childElement.innerHTML = tag.tag_name;
+            searchResult.appendChild(childElement);
+            const clickElement = document.getElementById(tag.id);
+            clickElement.addEventListener("click", () => {
+              document.getElementById("post_form_tag_name").value = clickElement.textContent;
+              clickElement.remove();
+            });  
+          });
+        };
     });
   };
 });
